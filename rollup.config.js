d74881939d507f5fcbe2ec2fb251f74401c4d40f
Jurij Zahn <jurij.zahn@otto.de>
2020-11-12 17:06:41 +0100

    chore(package): remove rollup plugin

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,6 @@ import typescript from "@rollup/plugin-typescript";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
-import excludeDependenciesFromBundle from "rollup-plugin-exclude-dependencies-from-bundle";
 
 export default {
   input: "./src/index.ts",
